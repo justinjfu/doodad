d0254af23ba640bde43024df26ce6e2e7cbd6515
Justin Fu <justinjfu@gmail.com>
2017-10-22 16:20:53 -0700

    Add gpu flags

--- doodad/ec2/aws_util.py
+++ doodad/ec2/aws_util.py
@@ -1,7 +1,9 @@
 import subprocess
 
-def s3_exists(bucket, path):
+def s3_exists(bucket, path, region=None):
     cmd = 'aws s3 ls s3://%s/%s' % (bucket, path)
+    if region is not None:
+        cmd += ' --region %s'%region
     try:
         result = subprocess.check_output(cmd, shell=True)
     except subprocess.CalledProcessError:
@@ -13,9 +15,9 @@ def s3_exists(bucket, path):
         return False
 
 
-def s3_upload(local_file_name, s3_bucket, s3_path, dry=False):
+def s3_upload(local_file_name, s3_bucket, s3_path, dry=False, region=None):
     remote_path = "s3://%s/%s" % (s3_bucket, s3_path)
-    upload_cmd = ["aws", "s3", "cp", local_file_name, remote_path]
+    upload_cmd = ["aws", "s3", "cp", '--region', region, local_file_name, remote_path]
     print(' '.join(upload_cmd))
     if not dry:
         subprocess.check_call(upload_cmd)
