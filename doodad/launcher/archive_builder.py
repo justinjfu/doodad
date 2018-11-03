472b1e3edf2a12eae25dafd9d9d8bc1eb8f1aceb
justinjfu <justinjfu@gmail.com>
2018-11-03 20:16:52 +0000

    add s3 dep functionality

--- doodad/launcher/archive_builder.py
+++ doodad/launcher/archive_builder.py
@@ -12,7 +12,8 @@ MAKESELF_HEADER_PATH = os.path.join(THIS_FILE_DIR, 'makeself-header.sh')
 
 def build_archive(output_file, 
                   launch_mode=None,
-                  mounts=()):
+                  mounts=(),
+                  verbose=False):
     # create a temporary work directory
     try:
         work_dir = tempfile.mkdtemp()
@@ -24,7 +25,7 @@ def build_archive(output_file,
         for mount in mounts:
             mount.dar_build_archive(deps_dir)
         
-        write_run_script(archive_dir, mounts)  #TODO:depends on launch mode
+        write_run_script(archive_dir, mounts, verbose=verbose)  #TODO:depends on launch mode
         write_metadata(archive_dir)
 
         # create the self-extracting archive
@@ -37,15 +38,21 @@ def write_metadata(arch_dir):
         f.write('doodad_version=%s\n' % doodad.__version__)
         f.write('unix_timestamp=%d\n' % time.time())
 
-def write_run_script(arch_dir, mounts):
+def write_run_script(arch_dir, mounts, verbose=False):
     runfile = os.path.join(arch_dir, 'run.sh')
     with open(runfile, 'w') as f:
-        f.write('echo Running Doodad Archive $1\n')
+        f.write('echo Running Doodad Archive [DAR] $1\n')
+        f.write('echo DAR build information:\n')
         f.write('cat ./METADATA\n')
-        # set up mounts
         # set up docker?
         for mount in mounts:
-            f.write(mount.dar_extract_command())
+            f.write('echo Mounting %s\n' % mount)
+            f.write(mount.dar_extract_command()+'\n')
+
+    if verbose:
+        print('[VERBOSE] Run script:')
+        with open(runfile) as f:
+            print(f.read())
     os.chmod(runfile, 0o777)
 
 def compile_archive(archive_dir, output_file):
@@ -53,7 +60,7 @@ def compile_archive(archive_dir, output_file):
     compile_cmd = compile_cmd.format(
         mkspath=MAKESELF_PATH,
         mkhpath=MAKESELF_HEADER_PATH,
-        name='dar',
+        name='DAR',
         archive_dir=archive_dir,
         output_file=output_file,
         run_script='./run.sh'
@@ -64,6 +71,6 @@ def compile_archive(archive_dir, output_file):
 if __name__ == "__main__":
     import doodad.launcher.mount
     mnt = doodad.launcher.mount.MountLocal(local_dir='./doodad')
-    build_archive('runfile.dar',
+    build_archive('runfile.dar', verbose=True,
     mounts=[mnt])
   
--- doodad/launcher/mount.py
+++ doodad/launcher/mount.py
@@ -12,6 +12,9 @@ import tarfile
 import tempfile
 from contextlib import contextmanager
 
+from doodad.ec2 import aws_util
+from doodad.launch import utils
+
 
 class Mount(object):
     """
@@ -25,12 +28,37 @@ class Mount(object):
         self.pythonpath = pythonpath
         self.read_only = not output
         self.mount_point = mount_point
+        self.__name = None
+        self.local_dir = None
 
     def dar_build_archive(self, deps_dir):
         raise NotImplementedError()
 
     def dar_extract_command(self):
         raise NotImplementedError()
+
+    @property
+    def name(self):
+        return self.__name
+
+    @contextmanager
+    def gzip(self, filter_ext=(), filter_dir=()):
+        """
+        Return filepath to a gzipped version of this directory for uploading
+        """
+        assert self.read_only
+        def filter_func(tar_info):
+            filt = any([tar_info.name.endswith(ext) for ext in filter_ext]) or \
+                   any([ tar_info.name.endswith('/'+ext) for ext in filter_dir])
+            if filt:
+                return None
+            return tar_info
+        with tempfile.NamedTemporaryFile('wb+', suffix='.tar') as tf:
+            # make a tar.gzip archive of directory
+            with tarfile.open(fileobj=tf, mode="w") as tar:
+                tar.add(self.local_dir, arcname=os.path.basename(self.local_dir), filter=filter_func)
+            tf.seek(0)
+            yield tf.name
 
 class MountLocal(Mount):
     def __init__(self, local_dir, mount_point=None, cleanup=True,
@@ -39,6 +67,7 @@ class MountLocal(Mount):
                 **kwargs):
         super(MountLocal, self).__init__(mount_point=mount_point, **kwargs)
         self.local_dir = os.path.realpath(os.path.expanduser(local_dir))
+        self.__name = self.local_dir.replace('/', '_')
         self.local_dir_raw = local_dir
         self.cleanup = cleanup
         self.filter_ext = filter_ext
@@ -48,33 +77,10 @@ class MountLocal(Mount):
             self.no_remount = True
         else:
             self.no_remount = False
-        #print('local_dir %s, mount_point %s(%s)' % (self.local_dir, self.mount_point, mount_point))
-
-    def create_if_nonexistent(self):
-        os.makedirs(self.local_dir, exist_ok=True)
-
-    @contextmanager
-    def gzip(self):
-        """
-        Return filepath to a gzipped version of this directory for uploading
-        """
-        assert self.read_only
-        def filter_func(tar_info):
-            filt = any([tar_info.name.endswith(ext) for ext in self.filter_ext]) or any([ tar_info.name.endswith('/'+ext) for ext in self.filter_dir])
-            if filt:
-                return None
-            return tar_info
-        with tempfile.NamedTemporaryFile('wb+', suffix='.tar') as tf:
-            # make a tar.gzip archive of directory
-            with tarfile.open(fileobj=tf, mode="w") as tar:
-                #tar.add(self.local_dir, arcname=os.path.splitext(os.path.basename(tf.name))[0], filter=filter_func)
-                tar.add(self.local_dir, arcname=os.path.basename(self.local_dir), filter=filter_func)
-            tf.seek(0)
-            yield tf.name
 
     def dar_build_archive(self, deps_dir):
         # make a symlink to deps dir
-        dep_dir = os.path.join(deps_dir, 'local', self.local_dir.replace('/', '_'))
+        dep_dir = os.path.join(deps_dir, 'local', self.name)
         os.makedirs(os.path.join(deps_dir, 'local'))
         #os.symlink(self.local_dir, dep_dir)
         shutil.copytree(self.local_dir, dep_dir)
@@ -82,7 +88,7 @@ class MountLocal(Mount):
     def dar_extract_command(self):
         # make a symlink to mount dir
         return 'ln -s ./deps/local/{dep_name} {mount}'.format(
-            dep_name=self.local_dir.replace('/', '_'),
+            dep_name=self.name,
             mount=self.mount_point
         )
 
@@ -103,19 +109,28 @@ class MountGitRepo(Mount):
 
 
 class MountS3(Mount):
-    def __init__(self, s3_path, s3_bucket=None, sync_interval=15, output=False,
-            include_types=('*.txt', '*.csv', '*.json', '*.gz', '*.tar', '*.log', '*.pkl'), **kwargs):
-        super(MountS3, self).__init__(**kwargs)
-        if s3_bucket is None:
-            # load from config
-            from doodad.ec2.autoconfig import AUTOCONFIG
-            s3_bucket = AUTOCONFIG.s3_bucket()
+    def __init__(self, 
+                region,
+                s3_bucket,
+                s3_path, 
+                local_dir=None,
+                sync_interval=15, 
+                output=False,
+                include_types=('*.txt', '*.csv', '*.json', '*.gz', '*.tar', '*.log', '*.pkl'), 
+                **kwargs):
+        super(MountS3, self).__init__(output=output, **kwargs)
+        # load from config
         self.s3_bucket = s3_bucket
         self.s3_path = s3_path
+        self.region = region
         self.output = output
         self.sync_interval = sync_interval
         self.sync_on_terminate = True
         self.include_types = include_types
+        self.__name = '%s.%s' % (self.s3_bucket, self.s3_path.replace('/', '.'))
+        if output is False:
+            assert local_dir is not None
+            self.local_dir = os.path.realpath(os.path.expanduser(local_dir))
 
     def __str__(self):
         return 'MountS3@s3://%s/%s'% (self.s3_bucket, self.s3_path)
@@ -124,16 +139,44 @@ class MountS3(Mount):
     def include_string(self):
         return ' '.join(['--include \'%s\''%type_ for type_ in self.include_types])
 
+    def s3_upload(self, filename, remote_filename=None, dry=False, check_exist=True):
+        if remote_filename is None:
+            remote_filename = os.path.basename(filename)
+        remote_path = 'doodad/mount/'+remote_filename
+        if check_exist:
+            if aws_util.s3_exists(self.s3_bucket, remote_path, region=self.region):
+                print('\t%s exists! ' % os.path.join(self.s3_bucket, remote_path))
+                return 's3://'+os.path.join(self.s3_bucket, remote_path)
+        return aws_util.s3_upload(filename, self.s3_bucket, remote_path, dry=dry,
+                         region=self.region)
+
     def dar_build_archive(self, deps_dir):
         # first upload directory to S3
-        # next, create the script to pull from S3
-        dep_dir = os.path.join(deps_dir, 's3', name)
+        with self.gzip() as gzip_file:
+            gzip_path = os.path.realpath(gzip_file)
+            file_hash = utils.hash_file(gzip_path)
+            s3_path = self.s3_upload(gzip_path, remote_filename=file_hash+'.tar')
+        self.path_on_remote = s3_path
+        self.local_file_hash = gzip_path
+
+        dep_dir = os.path.join(deps_dir, 's3', self.name)
         os.makedirs(os.path.join(deps_dir, 's3'))
-        raise NotImplementedError()
+
+        with open(os.path.join(dep_dir, 'extract.sh'), 'w') as f:
+            f.write('')
+            remote_tar_name = '/tmp/'+file_hash+'.tar'
+            mount_point =  os.path.join('/mounts', self.mount_point.replace('~/',''))
+            #remote_unpack_name = '/tmp/'+file_hash
+            f.write("aws s3 cp {s3_path} {remote_tar_name}\n".format(s3_path=s3_path, remote_tar_name=remote_tar_name))
+            f.write("mkdir -p {local_code_path}\n".format(local_code_path=mount_point))
+            f.write("tar -xvf {remote_tar_name} -C {local_code_path}\n".format(
+                local_code_path=mount_point,
+                remote_tar_name=remote_tar_name))
 
     def dar_extract_command(self):
         # execute the script to pull from S3
-        raise NotImplementedError()
+        if self.output is True:
+            raise NotImplementedError()
         return './deps/s3/{name}/extract.sh'.format(
-            name=None,
+            name=self.name,
         )
